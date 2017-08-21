a976937fd5a1ae99ac23e472e2e6bd47ee7efd65
Hami Abdi <abdih@users.noreply.github.com>
2017-08-21 12:48:53 -0600

    incorporated Twitter API

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,15 @@ $(function() {
 		$.ajax({
 			type: "GET",
 			url: "http://quotes.rest/qod.json",
-			success: localJsonCallback,
+			success: incorporateQuote,
 			dataType: "json"
 		});
 	});
 	*/
 
 	// JSONP approach
+
+	var displayingText;
 
 	$("#getQuote").on("click", function() {
 		$.ajax({
@@ -20,16 +22,19 @@ $(function() {
 			dataType: "jsonp",
 			success: localJsonpCallback
 		});
-
-		$(this).hide();
 	});
 
-	function localJsonCallback(json) {
-		$("#displayQuote").html('"' + json.contents.quotes[0].quote + '" -- ' + json.contents.quotes[0].author);
+	function incorporateQuote(data) {
+		displayingText = data.contents.quotes[0].quote + '" -- ' + data.contents.quotes[0].author;
+		$("#displayQuote").html(displayingText);
+		$("#getQuote").hide();
+		var tweetButton = $("<a></a>").attr({"class": "twitter-share-button btn btn-primary", "href": "https://twitter.com/intent/tweet?text=" + displayingText.replace(/ /g, "%20") + "&via=AbdiHami&related=abdihami&hashtags=RandomQuoteGenerator%2CSoftwareEngineerIsLife", "style": "margin-left: 45rem"});
+		tweetButton.text("Tweet");
+		$("#getQuote").parent().append(tweetButton)
+		$("#getQuote").remove();
 	}
 
-	function localJsonpCallback(json) {
-		var data = json.data;
-		$("#displayQuote").html('"' + data.contents.quotes[0].quote + '" -- ' + data.contents.quotes[0].author);
+	function JsonpQuote(jsonp) {
+		incorporateQuote(jsonp.data);
 	}
 });
